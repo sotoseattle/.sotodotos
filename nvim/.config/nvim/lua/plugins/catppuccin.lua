e03d5ce2a084e6f5f85236b684f76c94b723a88e
sotoseattle <sotoseattle@gmail.com>
2024-06-15 15:31:37 -0700

    twilight and obsidian

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,10 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      transparent_background = false,
+    })
     vim.cmd.colorscheme "catppuccin"
   end
 }
