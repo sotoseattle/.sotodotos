378a7c00ba36c7c77ecee909c86a2534b33d88a8
sotoseattle <sotoseattle@gmail.com>
2024-07-23 12:28:01 +0200

    floax for tmux and small tweaks to telescope keys

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -11,18 +11,18 @@ return {
 			vim.keymap.set("n", "<Leader>fz", builtin.live_grep, {})
 			vim.keymap.set("n", "<Leader>fb", builtin.buffers, {})
 
-			--vim.keymap.set("n", "<leader>ko", ":cd /Users/franciscosoto/vaults/sotolog<cr>")
-			vim.keymap.set("n", "<leader>kn", ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>")
+      -- OBSIDIAN STUFF
+			vim.keymap.set("n", "<leader>kn", ":ObsidianNewFromTemplate <cr>")
 			vim.keymap.set("n", "<leader>kf", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")
 			vim.keymap.set(
 				"n",
 				"<leader>ks",
-				':Telescope find_files search_dirs={"/Users/franciscosoto/vaults/sotolog"}<cr>'
+				':Telescope find_files search_dirs={"~/Dropbox/sotolog"}<cr>'
 			)
 			vim.keymap.set(
 				"n",
 				"<leader>kz",
-				':Telescope live_grep search_dirs={"/Users/franciscosoto/vaults/sotolog"}<cr>'
+				':Telescope live_grep search_dirs={"~/Dropbox/sotolog"}<cr>'
 			)
 		end,
 	},
@@ -30,6 +30,9 @@ return {
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({
+        defaults = {
+          path_display = { truncate = 1 }
+        },
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
